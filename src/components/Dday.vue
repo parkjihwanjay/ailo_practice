8ad52e10c9624009b14a423939320765c141e113
ParkJiHwan <cc6656@naver.com>
2019-08-18 16:33:37 +0900

    Dday 기능 추가

--- src/components/Dday.vue
+++ src/components/Dday.vue
@@ -2,6 +2,7 @@
   <div>
     <div>
       <input type="date" v-model="start_date" />
+      <div>{{Dday}}day</div>
     </div>
   </div>
 </template>
@@ -10,45 +11,40 @@ export default {
   name: "Dday",
   data() {
     return {
-      start_date: ""
+      start_date: "",
+      Dday: ""
     };
   },
   watch: {
     start_date: function(start_date) {
       //   console.log(start_date);
+      let Dday = 0;
 
       if (start_date < this.today) {
-        this.getDday(start_date, this.today);
+        Dday = this.getDday(start_date, this.today);
+        this.Dday = "-" + Dday;
       } else {
-        this.getDday(this.today, start_date);
+        Dday = this.getDday(this.today, start_date);
+        this.Dday = "+" + Dday;
       }
     }
   },
   methods: {
     getDday(start_date, finish_date) {
-      let start_date_split = start_date.split("-");
-      let finish_date_split = finish_date.split("-");
-      console.log(start_date_split);
-      console.log(finish_date_split);
+      let start_date_day = new Date(start_date);
+      let finish_date_day = new Date(finish_date);
+
+      let diff = Math.abs(finish_date_day.getTime() - start_date_day.getTime());
+      diff = Math.ceil(diff / (1000 * 3600 * 24));
+
+      console.log(diff);
+
+      return diff;
     }
   },
   computed: {
     today: function() {
       let today = new Date();
-      let dd = today.getDate();
-      let mm = today.getMonth() + 1;
-      let yyyy = today.getFullYear();
-
-      if (dd < 10) {
-        dd = "0" + dd;
-      }
-
-      if (mm < 10) {
-        mm = "0" + mm;
-      }
-
-      today = yyyy + "-" + mm + "-" + dd;
-
       return today;
     }
   }
