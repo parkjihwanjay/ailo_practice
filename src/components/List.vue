ff8212c0984c387224e07dab25f999e7d5d3f842
유지현 <jeehyun9808@naver.com>
2019-09-03 20:11:31 +0900

    일기 컴포넌트 및 24 플래너 컴포넌트 추가

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,82 +1,141 @@
 <template>
-  <div class="list_box">
-    <div class="list" v-for="(list, Bindex) in lists" :key="Bindex">
-      <span
-        class="list"
-        v-for="(block, Sindex) in list"
-        :key="(Bindex, Sindex)"
-        @dbclick="click(Bindex, Sindex)"
-        :class="{'clickon' : lists[Bindex][Sindex].status === 'clickon'}"
-      >{{Bindex}}, {{Sindex}}</span>
-    </div>
+  <div>
+    <table>
+      <tr>
+        <th v-for="minitue in minitues" :key="minitue">{{minitue}}</th>
+      </tr>
+      
+        <tr v-for="(one_hour, hindex) in one_day_planner" :key=hindex>
+          {{hindex}}
+          <td @mousedown="mouseDown(hindex, mindex)" @mouseup="mouseUp(hindex, mindex)"
+              class="border" 
+              v-for="(ten_minute, mindex) in one_hour"
+              :id = ten_minute.id
+              :key="(hindex, mindex)">
+          </td>
+        </tr>
+    </table>
 
-    <!-- <input type="text" v-if="list.status === 'clickon' " v-model="list.memo" />
-    <img
-      @click="trash(Bindex, Sindex)"
-      class="trash_img"
-      v-if="list.status === 'clickon'"
-      src="https://www.flaticon.com/premium-icon/icons/svg/484/484662.svg"
-    />-->
+    <div v-for="(color, index) in color_index" :key=index>
+      메모
+      {{color}}
+      <button @click="deleteColor(color)">삭제</button>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "Home",
-  data() {
+  data()
+  {
     return {
-      lists: []
-    };
+      minitues : [
+        '10',
+        '20',
+        '30',
+        '40',
+        '50',
+        '60',
+      ],
+      hour : 22,
+      one_day_planner : [],
+      color : [],
+      color_index : 0
+    }
   },
-  created: function() {
-    for (let i = 0; i < 24; i++) {
-      this.lists[i] = new Array();
-      for (let y = 0; y < 10; y++) {
-        this.lists[i].push({ status: "not_click" });
+  created : function(){
+    for(let i=0; i<this.hour; i++)
+    {
+      this.one_day_planner[i] = new Array();
+      for(let j=0; j<this.minitues.length; j++)
+      {
+        this.one_day_planner[i].push({
+          'id' : `${i}_${j}`
+          })
       }
     }
-
-    // console.log(this.lists);
   },
-  methods: {
-    click: function(Bindex, Sindex) {
-      console.log(Bindex, Sindex);
-      if (this.lists[Bindex][Sindex].status === "not_click") {
-        this.lists[Bindex][Sindex].status = "clickon";
-      } else {
-        this.lists[Bindex][Sindex].status = "not_click";
-      }
+  computed : {
+    hours : function(){
+      let hours = [];
+      for(let i=8 ; i<=26; i++)
+      {
+        let j = i%12;
+        let z;
 
-      if (this.lists[Bindex][Sindex].memo !== "")
-        this.lists[Bindex][Sindex].memo = "";
-      // console.log(this.lists[index]);
+        if(i===8)
+          z = "AM" + i;
+        else if(i===12)
+          z = "PM" + i;
+        else if(i===24)
+          z = "AM" + i;
+        else
+          z = j;
+        hours.push(z)
+      }
+      return hours;
+    }
+  },
+  methods : {
+    mouseDown(hindex, mindex){
+      this.color.push({
+        "start_index" : [hindex, mindex]
+      })
     },
-    trash: function(Bindex, Sindex) {
-      this.lists[Bindex][Sindex].status = "not_click";
-      this.lists[Bindex][Sindex].memo = "";
+    mouseUp(hindex, mindex){
+      this.color[this.color.length-1]['finish_index'] = [hindex, mindex]
+      this.colorPlanner();
+      
+    },
+    deleteColor(color_index)
+    {   
+      let elements = document.getElementsByClassName('colored_'+ color_index)
+      // console.log(elements[0])
+      for(let i=0; i<elements.length; i++)
+      {
+        elements[i].style.backgroundColor = 'white'
+      }
+    },
+    colorPlanner()
+    {
+      this.color_index++; 
+      let start_x = this.color[this.color.length-1]['start_index'][0]
+      let start_y = this.color[this.color.length-1]['start_index'][1]
+
+      let end_x = this.color[this.color.length-1]['finish_index'][0]
+      let end_y = this.color[this.color.length-1]['finish_index'][1]
+
+      while(1)
+      {
+        for(let i=start_y; i<=5; i++)
+        {
+          document.getElementById(start_x + '_' + i).style.backgroundColor = 'red';
+          document.getElementById(start_x + '_' + i).className = 'colored_' + this.color_index;
+
+          if(start_x === end_x && i === end_y)
+            return;
+        }
+        start_y = 0;
+        start_x++;
+      }
     }
   }
-};
+}
 </script>
 
 <style>
-.list_box {
-  width: 80%;
-  margin: 0 auto;
-}
-.list {
-  width: 100%;
+tr td{
   border: 1px solid black;
-  margin-bottom: 10px;
+  empty-cells: show;
+  padding: 10px;
 }
-.clickon {
-  background-color: red;
+
+table{
+  border-spacing: 0px;
 }
-.trash_img {
-  width: 4%;
-}
-.flex {
-  display: flex;
+
+.drag {
+  background: red;
 }
 </style>
-
